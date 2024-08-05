59e10cf513954bd1799477afa8a683d9a17f660d
ahmed-shorbagy <a7med.shorbagy@gmail.com>
2024-08-05 17:17:12 +0300

    Fix and Refactor Core Components in Invest App
    - Removed unnecessary import of 'package:dio/dio.dart' in 'faluire.dart' to clean up dependencies.
    - Deleted the entire 'ServerFaliure' class and its related methods in 'faluire.dart' to streamline error handling.
    - Updated 'app_router.dart' to include routing configuration using 'go_router' and added a route for 'SplashView'.
    - Simplified 'service_locator.dart' by removing unnecessary registrations of 'AuthRepo' and 'HomeRepo'.
    - Refactored 'main.dart' to initialize the app with a new class 'RoyalTop' and configure it to use the router for navigation.
    - Removed the default Flutter counter app code from 'main.dart' to focus on the core functionality of the Invest App.

--- lib/core/errors/faluire.dart
+++ lib/core/errors/faluire.dart
@@ -1,4 +1,3 @@
-import 'package:dio/dio.dart';
 
 abstract class Failure {
   final String errMessage;
@@ -55,50 +54,3 @@ class FirebaseFailure extends Failure {
   }
 }
 
-class ServerFaliure extends Failure {
-  ServerFaliure({required super.errMessage});
-  factory ServerFaliure.fromDioErr(DioException dioException) {
-    switch (dioException.type) {
-      case DioExceptionType.connectionTimeout:
-        return ServerFaliure(errMessage: 'Connection time out with the server');
-      case DioExceptionType.cancel:
-        return ServerFaliure(errMessage: 'Request to server was cancled');
-      case DioExceptionType.connectionError:
-        return ServerFaliure(errMessage: 'Connection time out with the server');
-      case DioExceptionType.sendTimeout:
-        return ServerFaliure(
-            errMessage:
-                'send time out with the server, please try again later!');
-      case DioExceptionType.receiveTimeout:
-        return ServerFaliure(
-            errMessage:
-                'recieve time out with the server, please try again later!');
-      case DioExceptionType.badResponse:
-        return ServerFaliure.fromResponce(
-            dioException.response!.statusCode!, dioException.response!.data);
-      case DioExceptionType.unknown:
-        if (dioException.message!.contains('SocketException')) {
-          return ServerFaliure(errMessage: 'No Internet Connection');
-        }
-        return ServerFaliure(
-            errMessage: 'Un Expected Error please try again later');
-      default:
-        return ServerFaliure(
-            errMessage: 'Oops there was an error, please try again later!');
-    }
-  }
-  factory ServerFaliure.fromResponce(int statusCode, dynamic responce) {
-    if (statusCode == 400 || statusCode == 401 || statusCode == 403) {
-      return ServerFaliure(errMessage: responce['error']['message']);
-    } else if (statusCode == 404) {
-      return ServerFaliure(
-          errMessage: 'Your request not found, please try again later!');
-    } else if (statusCode == 500) {
-      return ServerFaliure(
-          errMessage: 'Internal server proplem, please try again later!');
-    } else {
-      return ServerFaliure(
-          errMessage: 'Oops there was an error, please try again later!');
-    }
-  }
-}
--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,3 +1,10 @@
+import 'package:go_router/go_router.dart';
+import 'package:invest_app/features/splash/presentation/views/splash_view.dart';
+
 abstract class AppRouter {
   static const String kInitialView = '/';
+  static final router = GoRouter(routes: [
+
+    GoRoute(path: kInitialView, builder: (context, state) => const SplashView()),
+  ]);
 }
